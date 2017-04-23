345aee0e4aebd4edacdb8474dca4f5df237aefeb
JounQin <admin@1stg.me>
2017-04-23 17:14:32 +0800

    tiny simplification

--- src/routes.js
+++ src/routes.js
@@ -6,13 +6,13 @@ const resolve = (promise, callback) => promise.then(module => callback(null, mod
 export default {
   path: '/',
   getIndexRoute(partialNextState, callback) {
-    System.import('views/Home').then(module => callback(null, {component: module.default}))
+    import('views/Home').then(module => callback(null, {component: module.default}))
   },
   childRoutes: [
     {
       path: 'counter',
       getComponent(nextState, callback) {
-        resolve(System.import('views/Counter'), callback)
+        resolve(import('views/Counter'), callback)
       }
     }
   ]
