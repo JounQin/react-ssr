c342b9d06601dc4d268dfee1955f0f644905b032
JounQin <admin@1stg.me>
2017-06-01 09:42:33 +0800

    big refactor of server development experience

--- server/index.js
+++ server/index.js
@@ -5,11 +5,10 @@ import compress from 'koa-compress'
 import logger from 'koa-logger'
 import serve from 'koa-static'
 import _debug from 'debug'
-import runInVm from 'run-in-vm'
 
 import config, {globals, paths} from '../build/config'
 
-import intercept from './intercept'
+import {intercept, parseTemplate, createRunner} from './utils'
 
 const {__DEV__} = globals
 
@@ -20,53 +19,24 @@ const app = new Koa()
 app.use(compress())
 app.use(logger())
 
-function parseTemplate(template, contentPlaceholder = '<div id="app">') {
-  if (typeof template === 'object') {
-    return template
-  }
-
-  let i = template.indexOf('</head>')
-  const j = template.indexOf(contentPlaceholder)
-
-  if (j < 0) {
-    throw new Error(`Content placeholder not found in template.`)
-  }
-
-  if (i < 0) {
-    i = template.indexOf('<body>')
-    if (i < 0) {
-      i = j
-    }
-  }
-
-  return {
-    head: template.slice(0, i),
-    neck: template.slice(i, j),
-    tail: template.slice(j + contentPlaceholder.length)
-  }
-}
-
-let run, template
+let run, template, readyPromise
+let mfs // eslint-disable-line
 
 app.use(async (ctx, next) => {
-  const {req, res} = ctx
+  await readyPromise
 
-  if (!run || !template) {
-    ctx.status = 200
-    return res.end('waiting for compilation... refresh in a moment.')
-  }
+  const {res, url} = ctx
 
-  if (intercept(ctx)) {
+  if (intercept(ctx, {logger: __DEV__ && debug})) {
     await next()
     return
   }
 
   try {
-    const context = {url: req.url, template}
+    const context = {url, template}
     const {status, content} = await run(context)
     ctx.status = status
     res[status === 302 ? 'redirect' : 'end'](content)
-    res.end(content)
   } catch (e) {
     ctx.status = 500
     res.end('internal server error')
@@ -75,12 +45,14 @@ app.use(async (ctx, next) => {
 })
 
 if (__DEV__) {
-  require('./dev').default(app, {
-    bundleUpdated: bundle => (run = runInVm({bundle, runInNewContext: false})),
-    templateUpdated: temp => (template = parseTemplate(temp))
+  readyPromise = require('./dev').default(app, (bundle, {template: temp, fs}) => {
+    mfs = fs
+    run = createRunner(bundle)
+    template = parseTemplate(temp)
   })
 } else {
-  run = runInVm({bundle: require(paths.dist('ssr-bundle.json')), runInNewContext: false})
+  mfs = fs
+  run = createRunner(require(paths.dist('ssr-bundle.json')))
   template = parseTemplate(fs.readFileSync(paths.dist('index.html'), 'utf-8'))
   app.use(serve('dist'))
 }
